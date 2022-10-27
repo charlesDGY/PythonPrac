3cd4cb5fa2c2972f6db317cb96eecc438a37db53
Gaoda <edward.d.erlic@gmail.com>
2022-10-27 22:02:34 +0200

    10-27

--- test1.py
+++ test1.py
@@ -1,3 +1,49 @@
 print("Gaoda");
 print('o----')
 print(' ||||')
+print('*' * 15)
+#===============================================================================
+
+price = 10
+rating = 4.9
+name = 'Gaoda'
+is_published = False
+print(is_published)
+print(price)
+name = 'Gaoda'
+age = 20
+is_new = False
+is_new = True
+
+
+birth_year = '1998' 
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(type(age))
+print(age)
+
+#weight = input('Weight: ')
+#weight_kg = int(weight) * 0.45
+#print(weight_kg)
+#===============================================================================
+course = "Python's course for beginners"
+another = course[:]
+print(another)
+
+course = '''
+Hi Gaoda,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+'''
+print(course)
+
+
+name = 'jennifer'
+print(name[1:-1])
+
+
+count = '''"Gaoda" is heihei's name'''
+print(count[0:-1])
